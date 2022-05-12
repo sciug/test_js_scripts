47a71fbc12b9993bfc4e6763c07ecab9c82f0b37
sciug <gaetano.sciurti@libero.it>
2022-05-12 17:33:31 +0200

    anagram, palindrom, fizzbuzz

---
+++ countChar-start.js
@@ -0,0 +1,25 @@
+/**
+	* Scrivere una funzione che conti le ricorrenze dei caratteri all'interno di
+	* una parola. La funzione dovrà restituire un'oggetto con chiave la lettera e
+	* come valore il conteggio. Mostrare il risultato in console.
+	* Esempio
+	* countChar("hello") => {h: 1, e: 1, l: 2, o: 1}
+	*/
+
+let word = {}
+	function countChar(a){
+		let str = a.toLowerCase()
+		for (var i = 0; i < str.length; i++) {
+
+			word.str.charAt(i) 
+
+
+			
+
+
+			str.charAt(i)
+		  }
+		
+	}
+
+	countChar('ciao')
