18bd19c3d91c81c22a1580d229ac27665a9fc035
sciug <gaetano.sciurti@libero.it>
2022-05-12 18:01:36 +0200

    max cghar working

--- maxchar-start.js
+++ maxchar-start.js
@@ -4,3 +4,27 @@
 	* maxChar("abccccccd") => "c"
 	* maxChar("apple 12311111") => "1"
 	*/
+	let charBase= {}
+	
+	function maxChar(str){
+		
+		
+		str.toLowerCase()
+		.split('')
+		.forEach(char => {
+			if(char != ''){
+				if(charBase[char]==null){
+					charBase[char] = 1
+				}else{
+					charBase[char] +=1
+				}
+			}});
+
+
+			
+
+	}
+
+	console.log(maxChar('ciaoooo'))
+
+	console.log(charBase)
